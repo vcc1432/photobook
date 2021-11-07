aa65b4a2e3281262949a2993e7dbddd3f8d9df1c
vcc1432 <valerie.gersen@gmail.com>
2021-11-07 22:22:18 +0100

    Created Albums and AlbumsDetailPage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 import { auth } from './auth/auth';
+import { albumInfo } from './albums/albums';
 
 export default createStore({
-  modules: {
-    auth
-  }
-})
+	modules: {
+		auth,
+		albumInfo,
+	}
+});

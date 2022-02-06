d005369fde381e8fc5783621404adae3b5c459e8
vcc1432 <valerie.gersen@gmail.com>
2022-02-06 22:35:19 +0100

    Added screenshots, adjusted signup page

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -2,35 +2,41 @@
   <div class="flex justify-center">
     <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
       <form v-if="!confirmPassword" class="flex flex-col items-center" @submit.prevent="signUp">
-        <div class="flex flex-col user">
+        <div class="w-full flex flex-col user">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="username">User Name</label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
+            class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
             type="text"
             v-model="username"
             required
             id="username"
           />
         </div>
-        <div class="flex flex-col mt-2">
+        <div class="w-full flex flex-col mt-2">
           <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
+            class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
             type="password"
             id="password"
             required
             v-model="password"
           />
         </div>
-        <div class="flex flex-col mt-2 mb-3">
+        <div class="text-sm text-gray-500 mt-1">
+          Your password must be at least 8 characters long.
+        </div>
+        <div class="w-full flex flex-col mt-2 mb-3">
           <label class="block text-gray-700 text-sm font-bold" for="email">Email</label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
+            class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline invalid:border-red-500"
             type="email"
             id="email"
             required
             v-model="email"
           />
+          <div class="text-sm text-gray-500 mt-1">
+            In the next step, you will be asked to enter the code that has been sent to this email address.
+          </div>
         </div>
         <!-- eslint-disable -->
         <button class="btn-blue">Sign Up</button>
@@ -82,6 +88,7 @@ export default {
           email,
         });
         this.confirmPassword = true;
+        this.error = '';
       } catch (error) {
         this.error = { message: 'Something went wrong.' };
       }
